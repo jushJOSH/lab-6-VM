70b3282573a9bb958b8c75a22cadfc724934b22f
jushJOSH <reneres1337@gmail.com>
2020-11-25 18:20:38 +0700

    Добавьте файлы проекта.

---
+++ Lab - 6 - VM/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Lab___6___VM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.start_xn = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.start_y = new System.Windows.Forms.TextBox();
+            this.start_x = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.step = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.precision = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Left;
+            legend2.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Left;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.Legend = "Legend1";
+            series6.Name = "Эйлер";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.Legend = "Legend1";
+            series7.MarkerSize = 10;
+            series7.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Diamond;
+            series7.Name = "Рунге";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.Legend = "Legend1";
+            series8.Name = "Исправленный Эйлер";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series9.Legend = "Legend1";
+            series9.Name = "Адамс 5-го порядка";
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series10.Legend = "Legend1";
+            series10.Name = "Точное решение";
+            this.chart1.Series.Add(series6);
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Series.Add(series9);
+            this.chart1.Series.Add(series10);
+            this.chart1.Size = new System.Drawing.Size(776, 426);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = " Запуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.start_xn);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.start_y);
+            this.groupBox1.Controls.Add(this.start_x);
+            this.groupBox1.Location = new System.Drawing.Point(12, 215);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 106);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " Интервал";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = " Xn";
+            // 
+            // start_xn
+            // 
+            this.start_xn.Location = new System.Drawing.Point(63, 71);
+            this.start_xn.Name = "start_xn";
+            this.start_xn.Size = new System.Drawing.Size(131, 20);
+            this.start_xn.TabIndex = 4;
+            this.start_xn.Text = "10";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Y";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = " X";
+            // 
+            // start_y
+            // 
+            this.start_y.Location = new System.Drawing.Point(63, 45);
+            this.start_y.Name = "start_y";
+            this.start_y.Size = new System.Drawing.Size(131, 20);
+            this.start_y.TabIndex = 1;
+            this.start_y.Text = "0";
+            // 
+            // start_x
+            // 
+            this.start_x.Location = new System.Drawing.Point(63, 19);
+            this.start_x.Name = "start_x";
+            this.start_x.Size = new System.Drawing.Size(131, 20);
+            this.start_x.TabIndex = 0;
+            this.start_x.Text = "0";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.step);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.precision);
+            this.groupBox2.Location = new System.Drawing.Point(12, 327);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 77);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = " Параметры";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Шаг";
+            // 
+            // step
+            // 
+            this.step.Location = new System.Drawing.Point(63, 45);
+            this.step.Name = "step";
+            this.step.Size = new System.Drawing.Size(131, 20);
+            this.step.TabIndex = 8;
+            this.step.Text = "0,5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Точность";
+            // 
+            // precision
+            // 
+            this.precision.Location = new System.Drawing.Point(63, 19);
+            this.precision.Name = "precision";
+            this.precision.Size = new System.Drawing.Size(131, 20);
+            this.precision.TabIndex = 6;
+            this.precision.Text = "0,0001";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = " Лаба ку всем";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox start_y;
+        private System.Windows.Forms.TextBox start_x;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox start_xn;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox step;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox precision;
+    }
+}
+
---
+++ Lab - 6 - VM/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab___6___VM
+{
+    public partial class Form1 : Form
+    {
+        protected double X0, 
+                         Y0, 
+                         Xn, 
+                         Step, 
+                         Precision;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Начальная функция
+        private static double F(double x, double y)
+        {
+            return 3 - y - x;
+        }
+
+        // Точное решение
+        private static double f(double x)
+        {
+            return 4 - x - 4 * Math.Exp(-x);
+        }
+
+        // Отрисовка точного решения
+        public void DrawOriginalFunc()
+        {
+            chart1.Series[4].Name = "Точное решение";
+            chart1.Series[4].Points.Clear();
+            chart1.Series[4].IsVisibleInLegend = true;
+
+            double x = X0;
+
+            while (x <= Xn)
+            {
+                chart1.Series[4].Points.AddXY(x, f(x));
+                x += Step;
+            }
+        }
+
+        // Отрисовка Эйлера
+        public void DrawEuler()
+        {
+            chart1.Series[0].Name = "Эйлер";
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].IsVisibleInLegend = true;
+
+            double x = X0, 
+                   y = Y0;
+
+            while (x <= Xn)
+            {
+                chart1.Series[0].Points.AddXY(x, y);
+                y = y + Step * F(x, y);
+                x += Step;
+            }
+        }
+
+        // Отрисовка исправленного Эйлера
+        public void DrawFixedEuler()
+        {
+            chart1.Series[2].Name = "Исправленный Эйлер";
+            chart1.Series[2].IsVisibleInLegend = true;
+            chart1.Series[2].Points.Clear();
+
+            double x = X0, y = Y0;
+
+            while (x <= Xn)
+            {
+                chart1.Series[2].Points.AddXY(x, y);
+                double inCalc = Step * (F(x + Step, y + Step * F(x, y)) + F(x, y));
+                inCalc /= 2;
+                y += inCalc;
+                x += Step;
+            }
+        }
+
+        // Отрисовка Адамса
+        public void DrawAdams(int rang = 5)
+        {
+            chart1.Series[3].Name = "Адамс 5-го порядка";
+            chart1.Series[3].IsVisibleInLegend = true;
+            chart1.Series[3].Points.Clear();
+
+            int[,] coeffs = { {3, -1, 0, 0, 0 },
+                              {23,-16, 5, 0, 0 },
+                              {55, -59,37,-9, 0 },
+                              {1901, -2774, 2616, -1274, 251 } };
+
+            double dev = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                dev += coeffs[rang - 2, i];
+            }
+            double[] Fs = new double[rang];
+
+            double x = X0, y = Y0;
+            for (int i = 0; i < rang; i++)
+            {
+                chart1.Series[3].Points.AddXY(x, y);
+                Fs[rang - 1 - i] = F(x, y);
+                y += Step * F(x, y);
+                x += Step;
+            }
+            while (x <= Xn)
+            {
+                chart1.Series[3].Points.AddXY(x, y);
+                for (int i = rang - 2; i >= 0; i--)
+                {
+                    Fs[i + 1] = Fs[i];
+                }
+                Fs[0] = F(x, y);
+                double inCalc = 0;
+                for (int i = 0; i < rang; i++)
+                {
+                    inCalc += (Fs[i] * coeffs[rang - 2, i]);
+                }
+                inCalc *= (Step / dev);
+                y += inCalc;
+                x += Step;
+            }
+        }
+
+        // Отрисовка Рунге
+        public void DrawRunge()
+        {
+            chart1.Series[1].Name = "Метод Рунге–Кутты, Мерсона";
+            chart1.Series[1].IsVisibleInLegend = true;
+            chart1.Series[1].Points.Clear();
+
+            double[] k = new double[5];
+            double x = X0, y = Y0, prec = Precision;
+
+            while (x <= Xn)
+            {
+                chart1.Series[1].Points.AddXY(x, y);
+                double p = 0;
+                while (true)
+                {
+                    k[0] = Step * F(x, y);
+                    k[1] = Step * F(x + Step / 3, y + k[0] / 3);
+                    k[2] = Step * F(x + Step / 3, y + k[0] / 6 + k[1] / 6);
+                    k[3] = Step * F(x + Step / 2, y + k[0] / 8 + 3 * k[2] / 8);
+                    k[4] = Step * F(x + Step, y + k[0] / 2 - 3 * k[2] / 2 + 2 * k[3]);
+                    p = (2 * k[0] - 9 * k[2] + 8 * k[3] - k[4]) / 30;
+
+                    if (Math.Abs(Math.Round(p, 8)) > prec * Math.Abs(y)) 
+                        Step /= 2;
+                    else 
+                        break;
+                }
+
+                if (Math.Abs(Math.Round(p, 8)) <= (prec * Math.Abs(y) / 32)) 
+                    Step *= 2;
+                
+                y += k[0] / 6 + 2 * k[3] / 3 + k[4] / 6;
+                x += Step;
+            }
+        }
+
+        // Запуск
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(start_x.Text, out X0))
+            {
+                MessageBox.Show("Ошибка. Введённый X не является числом. Повторите ввод", "Ошибка ввода!");
+                start_x.Clear();
+                return;
+            }
+
+            if (!Double.TryParse(start_y.Text, out Y0))
+            {
+                MessageBox.Show("Ошибка. Введённый Y не является числом. Повторите ввод", "Ошибка ввода!");
+                start_x.Clear();
+                return;
+            }
+
+            if (!Double.TryParse(step.Text, out Step))
+            {
+                MessageBox.Show("Ошибка. Введённый шаг не является числом. Повторите ввод", "Ошибка ввода!");
+                start_x.Clear();
+                return;
+            }
+
+            if (!Double.TryParse(precision.Text, out Precision))
+            {
+                MessageBox.Show("Ошибка. Введённая точность не является числом. Повторите ввод", "Ошибка ввода!");
+                start_x.Clear();
+                return;
+            }
+
+            if (!Double.TryParse(start_xn.Text, out Xn))
+            {
+                MessageBox.Show("Ошибка. Введённый Xn не является числом. Повторите ввод", "Ошибка ввода!");
+                start_x.Clear();
+                return;
+            }
+
+            DrawAdams();
+            DrawEuler();
+            DrawFixedEuler();
+            DrawRunge();
+            DrawOriginalFunc();
+        }
+
+        // Загрузка формы
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Положение окна
+            Top = 20;
+            Left = 20;
+        }
+    }
+}
